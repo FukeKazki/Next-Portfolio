d1a1b0d399b38352088869387a231245c0904b5b
FukeKazki <kazkichi0906@gmail.com>
2020-05-02 18:21:19 +0900

    Add: GitPressの記事を取得の実装

---
+++ src/lib/gitPress.ts
@@ -0,0 +1,32 @@
+import fetch from 'isomorphic-unfetch'
+import XMLParser from 'xml2js'
+
+const GitPressAtomURL = 'https://gitpress.io/static/6495a013a74997b89b08be0a51c8eb77bf6652eb934985f92035eb8b75b813d7/atom.xml'
+
+const fetchGitPressXML = async (): Promise<string | null> => {
+    try {
+        const response = await fetch(GitPressAtomURL)
+        const xml = await response.text()
+
+        return xml
+    } catch (err) {
+        console.error('GitPress fetch Error', err)
+
+        return null
+    }
+}
+
+export const getGitPressArticles = async (): Promise<object[]> => {
+    const xml = await fetchGitPressXML()
+    const parsed_xml = await XMLParser.parseStringPromise(xml).catch(null)
+    const articles = parsed_xml.feed.entry.map(article => {
+        // article contained {title, summary, id, link, updated}
+        return {
+            title: article.title.toString(),
+            link: article.id.toString(),
+            updated: article.updated.toString(),
+        }
+    })
+
+    return articles
+}
